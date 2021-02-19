6798da713403ed6fb317233e4183afd7684a381b
bnjmnhssnn <info@benjamin-hosseinian.de>
2021-02-19 10:00:54 +0000

    Konfigurator kann jetzt seinen Zustand aus der Session wiederherstellen

--- classes/Configurator.php
+++ classes/Configurator.php
@@ -2,31 +2,37 @@
 namespace Grav\Plugin\IServConfigurator;
 
 use Grav\Common\Config\Config;
+use Grav\Common\Session;
+
 
 class Configurator
 {
     protected $steps, $choices, $state;
 
-    public function __construct(Config $config)
+    public function __construct(Config $config, Session $session)
     {
         $this->configured_steps = $config->get('plugins.configurator.configurator_tree.steps');
         $this->configured_choices = $config->get('plugins.configurator.configurator_choices');
 
         $step_index = 1;
-        $this->state = [
-            'steps' => array_map(
-                function($step) use (&$step_index) {
-                    $res = [
-                        'id' => $step_index,
-                        'visible' => ($step_index === 1),
-                        'choice' => NULL
-                    ];
-                    $step_index++;
-                    return $res;
-                },
-                $this->configured_steps
-            )
-        ];
+        if (empty($session->configurator)) {
+            $this->state = [
+                'steps' => array_map(
+                    function($step) use (&$step_index) {
+                        $res = [
+                            'id' => $step_index,
+                            'visible' => ($step_index === 1),
+                            'choice' => NULL
+                        ];
+                        $step_index++;
+                        return $res;
+                    },
+                    $this->configured_steps
+                )
+            ];
+        } else {
+            $this->state = $session->configurator;    
+        }
     }
 
     public function getState()
--- tests/IServConfiguratorTest.php
+++ tests/IServConfiguratorTest.php
@@ -6,23 +6,11 @@ use Symfony\Component\Yaml\Yaml;
 
 final class IServConfiguratorTest extends TestCase
 {
-    public function testInitialState() : void
+    public function testInitialStateWhenSessionEmpty() : void
     {
-        $config_mock = $this->getMock('Grav\Common\Config\Config', ['get']);
-        $config_mock->method('get')->will(
-            $this->returnCallBack(
-                function($path) {
-                    switch($path) {
-                        case 'plugins.configurator.configurator_tree.steps':
-                            return Yaml::parse(file_get_contents(__DIR__ . '/configurator_tree.mock.yaml'));
-                        case 'plugins.configurator.configurator_choices':
-                            return Yaml::parse(file_get_contents(__DIR__ . '/configurator_choices.mock.yaml'));   
-                    }    
-                }
-            )    
-        );
-        $session_mock = $this->getMock('Grav\Common\Session', []);
-        $configurator = new Configurator($config_mock);
+        $config_mock = $this->getConfigMock();
+        $session_mock = $this->getSessionMock();
+        $configurator = new Configurator($config_mock, $session_mock);
         $expected_state = [
             'steps' => [
                 ['id' => 1, 'visible' => true, 'choice' => NULL],
@@ -32,14 +20,58 @@ final class IServConfiguratorTest extends TestCase
                 ['id' => 5, 'visible' => false, 'choice' => NULL]
             ]
         ];
-        $this->assertEquals($expected_state, $configurator->getState());    }
+        $this->assertEquals($expected_state, $configurator->getState());
+    }
 
-    protected function getMock(string $classname, array $methods)
+    public function testRestoreStateFromSession() : void
     {
-        $mock = $this->getMockBuilder($classname)
-            ->setMethods($methods)
-            ->getMock();
+        $config_mock = $this->getConfigMock();
+        $session_mock = $this->getSessionMock();
+        $session_mock->configurator = [
+            'steps' => [
+                ['id' => 1, 'visible' => false, 'choice' => 1],
+                ['id' => 2, 'visible' => true, 'choice' => NULL],
+                ['id' => 3, 'visible' => false, 'choice' => NULL],
+                ['id' => 4, 'visible' => false, 'choice' => NULL],
+                ['id' => 5, 'visible' => false, 'choice' => NULL]
+            ]
+        ];
+        $configurator = new Configurator($config_mock, $session_mock);
+        $expected_state = [
+            'steps' => [
+                ['id' => 1, 'visible' => false, 'choice' => 1],
+                ['id' => 2, 'visible' => true, 'choice' => NULL],
+                ['id' => 3, 'visible' => false, 'choice' => NULL],
+                ['id' => 4, 'visible' => false, 'choice' => NULL],
+                ['id' => 5, 'visible' => false, 'choice' => NULL]
+            ]
+        ];
+        $this->assertEquals($expected_state, $configurator->getState());
+    }
+
+    protected function getConfigMock()
+    {
+        $mock = $this->getMockBuilder('Grav\Common\Config\Config')->setMethods(['get'])->getMock();
+        $mock
+            ->method('get')
+            ->will(
+                $this->returnCallBack(
+                    function($path) {
+                        switch($path) {
+                            case 'plugins.configurator.configurator_tree.steps':
+                                return Yaml::parse(file_get_contents(__DIR__ . '/configurator_tree.mock.yaml'));
+                            case 'plugins.configurator.configurator_choices':
+                                return Yaml::parse(file_get_contents(__DIR__ . '/configurator_choices.mock.yaml'));   
+                        }    
+                    }
+                )    
+            );
         return $mock;
+    }
+
+    protected function getSessionMock()
+    {
+        return $this->getMockBuilder('Grav\Common\Session')->getMock();
     }
 
 }
