2ffce6479b1f65b7fecd9dc6412d5247a4a8c6c7
bnjmnhssnn <info@benjamin-hosseinian.de>
2021-02-19 09:33:33 +0000

    Grav Konfiguration kann jetzt gemocked werden

--- classes/Configurator.php
+++ classes/Configurator.php
@@ -1,6 +1,12 @@
 <?php
 namespace Grav\Plugin\IServConfigurator;
 
+use Grav\Common\Config\Config;
+
 class Configurator
 {
+    public function __construct(Config $config)
+    {
+
+    }
 }
--- tests/IServConfiguratorTest.php
+++ tests/IServConfiguratorTest.php
@@ -1,13 +1,39 @@
 <?php
 use PHPUnit\Framework\TestCase;
 use Grav\Plugin\IServConfigurator\Configurator;
+use Symfony\Component\Yaml\Yaml;
+
 
 final class IServConfiguratorTest extends TestCase
 {
-    public function testDummy()
+    public function testInstanciation() : void
     {
-        $configurator = new Configurator();
-        $this->assertTrue(true);
+        $config_mock = $this->getMock('Grav\Common\Config\Config', ['get']);
+        $config_mock->method('get')->will(
+            $this->returnCallBack(
+                function($path) {
+                    switch($path) {
+                        case 'plugins.configurator.configurator_tree.steps':
+                            return Yaml::parse(file_get_contents(__DIR__ . '/configurator_tree.mock.yaml'));
+                        case 'plugins.configurator.configurator_choices':
+                            return Yaml::parse(file_get_contents(__DIR__ . '/configurator_choices.mock.yaml'));   
+                    }    
+                }
+            )    
+        );
+
+        $session_mock = $this->getMock('Grav\Common\Session', []);
+
+        $this->expectNotToPerformAssertions();
+        $tree = new Configurator($config_mock);
+    }
+
+    protected function getMock(string $classname, array $methods)
+    {
+        $mock = $this->getMockBuilder($classname)
+            ->setMethods($methods)
+            ->getMock();
+        return $mock;
     }
 
 }
