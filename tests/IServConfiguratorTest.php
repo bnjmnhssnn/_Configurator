50b292f3f8a973a107701a1e5477f2daff19f852
bnjmnhssnn <info@benjamin-hosseinian.de>
2021-02-19 09:46:00 +0000

    Konfigurator legt seinen Eingangszustand anhand der Konfiguration fest

--- tests/IServConfiguratorTest.php
+++ tests/IServConfiguratorTest.php
@@ -6,7 +6,7 @@ use Symfony\Component\Yaml\Yaml;
 
 final class IServConfiguratorTest extends TestCase
 {
-    public function testInstanciation() : void
+    public function testInitialState() : void
     {
         $config_mock = $this->getMock('Grav\Common\Config\Config', ['get']);
         $config_mock->method('get')->will(
@@ -21,12 +21,18 @@ final class IServConfiguratorTest extends TestCase
                 }
             )    
         );
-
         $session_mock = $this->getMock('Grav\Common\Session', []);
-
-        $this->expectNotToPerformAssertions();
-        $tree = new Configurator($config_mock);
-    }
+        $configurator = new Configurator($config_mock);
+        $expected_state = [
+            'steps' => [
+                ['id' => 1, 'visible' => true, 'choice' => NULL],
+                ['id' => 2, 'visible' => false, 'choice' => NULL],
+                ['id' => 3, 'visible' => false, 'choice' => NULL],
+                ['id' => 4, 'visible' => false, 'choice' => NULL],
+                ['id' => 5, 'visible' => false, 'choice' => NULL]
+            ]
+        ];
+        $this->assertEquals($expected_state, $configurator->getState());    }
 
     protected function getMock(string $classname, array $methods)
     {
